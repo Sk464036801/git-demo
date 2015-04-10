9b06d1f172758486649fecfe9ea1ca95cf9ccd57
shangke <xiaoshang_ke@sina.cn>
2015-04-10 20:37:30 +0800

    rebase commit test.

--- git-demo/src/main/java/com/app/git/demo/Example2.java
+++ git-demo/src/main/java/com/app/git/demo/Example2.java
@@ -5,7 +5,7 @@ public class Example2 {
 	public static void main(String[] args) {
 		System.out.println("1111");
 		System.out.println("git repository edit commit.");
-		System.out.println("git repository edit commit2.");
+		System.out.println("git rebase test commit.");
 	}
 
 }

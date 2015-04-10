310e582ceaa489d5aab7e93a1938d604a370a639
shangke <xiaoshang_ke@sina.cn>
2015-04-10 20:49:29 +0800

    feature commit.

--- git-demo/src/main/java/com/app/git/demo/Example.java
+++ git-demo/src/main/java/com/app/git/demo/Example.java
@@ -5,7 +5,7 @@ public class Example {
 	public static void main(String[] args) {
 		System.out.println("Hello World ..");
 		System.out.println("2222222222222");
-		System.out.println("Feature Test cmmit");
+		System.out.println("Feature- newFun");
 	}
 
 }

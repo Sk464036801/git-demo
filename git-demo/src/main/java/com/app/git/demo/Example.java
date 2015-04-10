ae5a456ac5d102f6a3b45e2becd12dc0f761eb0e
shangke <xiaoshang_ke@sina.cn>
2015-04-10 20:44:53 +0800

    222222

--- git-demo/src/main/java/com/app/git/demo/Example.java
+++ git-demo/src/main/java/com/app/git/demo/Example.java
@@ -4,7 +4,7 @@ public class Example {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World ..");
-		System.out.println("Hello World 111111..");
+		System.out.println("2222222222222");
 	}
 
 }

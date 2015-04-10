c338f140542176607ba9b85f9dd2bc67d7b44404
shangke <xiaoshang_ke@sina.cn>
2015-04-10 20:03:54 +0800

    first commit 1.

---
+++ git-demo/src/main/java/com/app/git/demo/TimeDemo.java
@@ -0,0 +1,23 @@
+package com.app.git.demo;
+
+import java.util.Calendar;
+
+public class TimeDemo {
+	
+	public static void main(String[] args) {
+		
+		Calendar cal = Calendar.getInstance();
+		System.out.println(cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+		System.out.println(cal.getActualMinimum(Calendar.DAY_OF_MONTH));
+		
+		long start = cal.getTimeInMillis();
+		cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+		
+		long end = cal.getTimeInMillis();
+		
+		long sectime = (end - start)/(1000*60*60*24);
+		
+		System.out.println(sectime);
+	}
+
+}

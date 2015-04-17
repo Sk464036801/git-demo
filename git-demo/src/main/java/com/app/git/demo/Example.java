562029c79c34233a565ddbb4cf187a2dd6006eec
shangke <xiaoshang_ke@sina.cn>
2015-04-17 18:08:28 +0800

    commit

--- git-demo/src/main/java/com/app/git/demo/Example.java
+++ git-demo/src/main/java/com/app/git/demo/Example.java
@@ -1,11 +1,17 @@
 package com.app.git.demo;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Example {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World ..");
 		System.out.println("2222222222222");
 		System.out.println("Feature- newFun0001");
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+		System.out.println(format.format(new Date()));
 	}
 
 }
---
+++ git-demo/src/main/java/com/app/git/demo/Example3.java
@@ -0,0 +1,11 @@
+package com.app.git.demo;
+
+public class Example3 {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+		System.out.println("33333");
+	}
+
+}

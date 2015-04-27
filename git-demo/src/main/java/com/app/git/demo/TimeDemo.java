13400997ab8b2e92d342959fa5243ccdfc144e07
shangke <xiaoshang_ke@sina.cn>
2015-04-27 12:17:25 +0800

    Merge branch 'dev-1'
    Conflicts:
    	git-demo/src/main/java/com/app/git/demo/TimeDemo.java

--- git-demo/src/main/java/com/app/git/demo/TimeDemo.java
+++ git-demo/src/main/java/com/app/git/demo/TimeDemo.java
@@ -36,7 +36,6 @@ public class TimeDemo {
 		System.out.println("test2XOXO remote repository update commit 5...");
 		System.out.println("test3 local repository commit 6...");
 		System.out.println("testXXX local repository commit 6...");
-		
 	};
 
 	public String getAddr(String addr) {

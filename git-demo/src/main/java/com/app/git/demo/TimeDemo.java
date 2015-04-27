3ede5436f3c1829c9950c1b775b8a1eb88936ef2
shangke <xiaoshang_ke@sina.cn>
2015-04-27 11:27:47 +0800

    dev-1 local update delete commit 7. dev-1 local update delete commit 5.

--- git-demo/src/main/java/com/app/git/demo/TimeDemo.java
+++ git-demo/src/main/java/com/app/git/demo/TimeDemo.java
@@ -36,11 +36,7 @@ public class TimeDemo {
 		
 		System.out.println("test3 local repository commit 2...");
 		
-		System.out.println("dev-1-XX local repository commit 5...");
 		
-		System.out.println("dev-1-OO local repository commit 6...");
-		
-		System.out.println("dev-1-OO local repository commit 7...");
 		
 	};
 

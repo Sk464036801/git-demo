dc50ebc90fc845c792e17683f6a5fb17a1f485f3
shangke <xiaoshang_ke@sina.cn>
2015-09-09 17:34:33 +0800

    develop branche modify commit

--- git-demo/src/main/java/com/app/git/demo/App.java
+++ git-demo/src/main/java/com/app/git/demo/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(" it is master branch 001");
+        System.out.println(" it is master branch 001 develop modify");
         
         System.out.println("develop feature branche commit 001");
         

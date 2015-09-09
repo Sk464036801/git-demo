b2efdae00f466f1e563d28dce08876333e621d2f
shangke <xiaoshang_ke@sina.cn>
2015-09-09 18:02:37 +0800

    delete line code.

--- git-demo/src/main/java/com/app/git/demo/App.java
+++ git-demo/src/main/java/com/app/git/demo/App.java
@@ -12,7 +12,6 @@ public class App
         System.out.println(" it is master branch 001");
         
         System.out.println("develop feature branche commit 001");
-        System.out.println("develop feature branche commit 002");
         
 
     }

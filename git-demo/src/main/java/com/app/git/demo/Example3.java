79e4c13e77de103d1c747b30f1f030c6af513c3f
shangke <xiaoshang_ke@sina.cn>
2015-09-10 15:08:07 +0800

    develop feature test commit 013

--- git-demo/src/main/java/com/app/git/demo/Example3.java
+++ git-demo/src/main/java/com/app/git/demo/Example3.java
@@ -4,13 +4,7 @@ public class Example3 {
 	
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		
 		System.out.println("33333");
-		
-		System.out.println("cherry-pick 004 function");
-		System.out.println("cherry-pick 004 function 0001");
-		System.out.println("cherry-pick 004 function 0002");
-		System.out.println("cherry-pick 004 function 0003");
 		System.out.println("cherry-pick 004 function 0012");
 	}
 
--- git-demo/src/main/java/com/app/git/demo/FeatureDemo.java
+++ git-demo/src/main/java/com/app/git/demo/FeatureDemo.java
@@ -5,11 +5,6 @@ public class FeatureDemo {
 		System.out.println("feature test commit 001");
 		System.out.println("feature test commit 002");
 		System.out.println("feature test commit 003");
-		System.out.println("feature test develop commit 003");
-		System.out.println("feature test develop commit 004");
-		System.out.println("feature test develop commit 005");
-		System.out.println("feature test develop commit 006");
-		System.out.println("feature test develop commit 007");
 		System.out.println("cherry-pick 004 function 00010");
 	}
 

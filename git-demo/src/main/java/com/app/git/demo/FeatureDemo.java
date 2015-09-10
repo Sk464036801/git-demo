361873f28b8201b1c58fb396e2f78a9c5a33aee3
shangke <xiaoshang_ke@sina.cn>
2015-09-10 14:25:56 +0800

    develop feature test commit 005

--- git-demo/src/main/java/com/app/git/demo/FeatureDemo.java
+++ git-demo/src/main/java/com/app/git/demo/FeatureDemo.java
@@ -7,6 +7,7 @@ public class FeatureDemo {
 		System.out.println("feature test commit 003");
 		System.out.println("feature test develop commit 003");
 		System.out.println("feature test develop commit 004");
+		System.out.println("feature test develop commit 005");
 	}
 
 }

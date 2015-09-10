800c56e05be6dbb6d841cc57b672a11aa4f72e18
shangke <xiaoshang_ke@sina.cn>
2015-09-10 15:18:41 +0800

    develop feature test commit 014

--- git-demo/src/main/java/com/app/git/demo/Example3.java
+++ git-demo/src/main/java/com/app/git/demo/Example3.java
@@ -5,6 +5,7 @@ public class Example3 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("33333");
+		System.out.println("feature-01  commit 001");
 	}
 
 }

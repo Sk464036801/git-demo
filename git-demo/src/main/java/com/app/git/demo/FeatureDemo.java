918e8a91cc6328337cb0d130871a8d772d7b9eb6
shangke <xiaoshang_ke@sina.cn>
2015-09-10 11:24:43 +0800

    add Feature Demo file commit 003

--- git-demo/src/main/java/com/app/git/demo/FeatureDemo.java
+++ git-demo/src/main/java/com/app/git/demo/FeatureDemo.java
@@ -4,6 +4,7 @@ public class FeatureDemo {
 	public static void main(String[] args) {
 		System.out.println("feature test commit 001");
 		System.out.println("feature test commit 002");
+		System.out.println("feature test commit 003");
 	}
 
 }
